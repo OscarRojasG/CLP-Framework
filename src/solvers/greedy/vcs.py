018950748bb268050735802e66b7ca3a5e76b51f
OscarRojasG <oscar.rojas.g@mail.pucv.cl>
2026-03-21 14:06:24 -0300

    add vcs executable

--- src/solvers/greedy/vcs.py
+++ src/solvers/greedy/vcs.py
@@ -1,11 +1,21 @@
-from solvers.bs.bsg import BSG_Solver
+import subprocess
+from settings import VCS_SOLVER_PATH, INSTANCE_FOLDER
 from solvers.greedy.greedy_solver import GreedySolver
 
 class VCSSolver(GreedySolver):
     def __init__(self):
         super().__init__("VCS")
-        self.bsg = BSG_Solver()
 
-    # TODO: Crear ejecutable para VCS        
     def solve(self, instance_file, instance_number, min_fr) -> int:
-        return self.bsg.solve(instance_file, instance_number, 1, min_fr)
+        # Ejecutar el proceso y capturar la salida
+        proc = subprocess.run(
+            [VCS_SOLVER_PATH, INSTANCE_FOLDER / instance_file, "-i", str(instance_number), f"--min_fr={min_fr}"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
+            check=True,
+            text=True
+        )
+
+        line = proc.stdout.strip().splitlines()[-1]
+        volume = float(line)
+        return volume
